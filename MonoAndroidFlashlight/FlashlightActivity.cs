f2f74d768b4911e9d58e66fabebdfcc870cbc217
Adam Patridge <patridge@gmail.com>
2012-07-11 13:19:10 -0600

    Initial code import and readme.

---
+++ MonoAndroidFlashlight/FlashlightActivity.cs
@@ -0,0 +1,123 @@
+﻿namespace MonoAndroidFlashlight {
+    using System.Collections.Generic;
+    using Android.App;
+    using Android.Hardware;
+    using Android.OS;
+    using Android.Views;
+    using Android.Widget;
+
+    // NOTE: If you are grabbing this code, make sure you also copy the camera permission from /Properties/AndroidManifest.xml.
+    //       <uses-permission android:name="android.permission.CAMERA" />
+    [Activity(Label = "Flashlight", MainLauncher = true, Icon = "@drawable/icon")]
+    public class FlashlightActivity : Activity, ISurfaceHolderCallback {
+        protected Camera _Camera = null;
+        protected IList<string> _CameraSupportedFlashModes = null;
+        protected SurfaceView _SurfaceView = null;
+        protected ISurfaceHolder _SurfaceHolder = null;
+        protected const string FlashlightOnMode = Camera.Parameters.FlashModeTorch;
+        protected const string FlashlightOffMode = Camera.Parameters.FlashModeOff;
+        private bool IsPreviewing = false;
+
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            StartCamera();
+            _SurfaceView = FindViewById<SurfaceView>(Resource.Id.SurfaceView);
+            _SurfaceHolder = _SurfaceView.Holder;
+            _SurfaceHolder.AddCallback(this);
+            _SurfaceHolder.SetType(SurfaceType.PushBuffers);
+
+            ImageView flashlightToggleButton = FindViewById<ImageView>(Resource.Id.FlashTorch);
+            flashlightToggleButton.Click += (sender, args) => {
+                if (FlashlightOnMode != GetCameraFlashMode()) {
+                    TurnFlashOn();
+                }
+                else {
+                    TurnFlashOff();
+                }
+            };
+        }
+        protected override void OnStop() {
+            StopCamera();
+            base.OnStop();
+        }
+        protected override void OnStart() {
+            base.OnStart();
+            StartCamera();
+        }
+        protected override void OnResume() {
+            base.OnResume();
+            TurnFlashOn();
+        }
+        protected override void OnPause() {
+            base.OnPause();
+            TurnFlashOff();
+        }
+        public void SurfaceChanged(ISurfaceHolder holder, Android.Graphics.Format format, int width, int height) { }
+        public void SurfaceCreated(ISurfaceHolder holder) {
+            _Camera.SetPreviewDisplay(holder);
+        }
+        public void SurfaceDestroyed(ISurfaceHolder holder) { }
+
+        protected void StartCamera() {
+            if (_Camera == null) {
+                _Camera = Camera.Open();
+                _CameraSupportedFlashModes = _CameraSupportedFlashModes ?? _Camera.GetParameters().SupportedFlashModes;
+                if (_CameraSupportedFlashModes == null || !_CameraSupportedFlashModes.Contains(FlashlightOnMode) || !_CameraSupportedFlashModes.Contains(FlashlightOffMode)) {
+                    StopCamera();
+                }
+            }
+        }
+        protected void StartPreviewing() {
+            if (_Camera == null) {
+                StartCamera();
+            }
+            if (_Camera != null && !IsPreviewing) {
+                _Camera.StartPreview();
+                IsPreviewing = true;
+            }
+        }
+        protected void StopPreviewing() {
+            if (_Camera != null && IsPreviewing) {
+                _Camera.StopPreview();
+            }
+            IsPreviewing = false;
+        }
+        protected void StopCamera() {
+            StopPreviewing();
+            if (_Camera != null) {
+                _Camera.Release();
+                _Camera = null;
+            }
+        }
+        protected string GetCameraFlashMode(Camera.Parameters cameraParameters = null) {
+            string mode = null;
+            if (_Camera != null) {
+                if (cameraParameters == null) {
+                    cameraParameters = _Camera.GetParameters();
+                }
+                mode = cameraParameters.FlashMode;
+            }
+            return mode;
+        }
+        protected void SetCameraFlashMode(string newMode) {
+            if (_Camera != null) {
+                Camera.Parameters cameraParameters = _Camera.GetParameters();
+                if (newMode != GetCameraFlashMode(cameraParameters)) {
+                    cameraParameters.FlashMode = newMode;
+                    _Camera.SetParameters(cameraParameters);
+                }
+            }
+        }
+        protected void TurnFlashOn() {
+            StartPreviewing();
+            SetCameraFlashMode(FlashlightOnMode);
+        }
+        protected void TurnFlashOff() {
+            StopPreviewing();
+            SetCameraFlashMode(FlashlightOffMode);
+        }
+    }
+}
---
+++ README.markdown
@@ -0,0 +1,18 @@
+###Description
+
+A simple sample flashlight app created in Mono for Android.
+
+###Background
+
+My new Samsung Galaxy Nexus didn't come with a flashlight app like the HTC Incredible [1] that it replaced. With an inherent distrust of free Android apps, I decided to make my own. Many Java tutorials and development quirks later (first versiion worked great on Android 2.2 but failed miserably on Android 4.0), and I had the start of a useful app.
+
+###Coming Soon
+
+1. Toggle image for on/off state.
+1. Try to prevent "blinking" when rotating the device.
+1. Show white screen as pseudo-flashlight for devices without a camera flash.
+1. Try to figure out a way to use the flash even when the device doesn't support torch mode.
+
+###References
+
+Power logo used for this app was found via The Noun Project (under public domain).
